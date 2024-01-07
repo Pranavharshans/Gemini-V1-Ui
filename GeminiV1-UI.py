c2cf7c39c390f0e46649c60fd8fbd0d67672cd62
Pranavharshans <133397331+Pranavharshans@users.noreply.github.com>
2024-01-07 13:11:57 +0530

    Add files via upload

---
+++ GeminiV1-UI.py
@@ -0,0 +1,27 @@
+import textwrap
+import streamlit as st
+import google.generativeai as genai
+
+# INSERT API KEY HERE
+genai.configure(api_key='')
+
+def format_text(text):
+    return textwrap.fill(text, width=80)
+
+def main():
+    st.title("Gemini Text Generation")
+
+    model = genai.GenerativeModel('gemini-pro')
+
+    user_input = st.text_input("Enter a prompt:")
+    
+    if st.button("Generate"):
+        if user_input.upper() == 'END GEMINI':
+            st.text("Exiting Gemini...")
+        else:
+            response = model.generate_content(user_input)
+            formatted_text = format_text(response.text)
+            st.text(formatted_text)
+
+# Run the main function
+main()
